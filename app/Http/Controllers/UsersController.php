26e2664ca7f7aae983bf6863bd35a9e11a7b30bc
MacBook <alonasoroka1993@gmail.com>
2018-04-03 12:19:18 +0300

    redirect in home page

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,11 +4,17 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UsersController extends Controller
 {
     public function profile(Request $request){
         $user_id = $request->route('id');
+        $auth_id = Auth::user()->id;
+
+        if($user_id == $auth_id){
+            return \redirect(route('home'));
+        }
 
         $user = User::find($user_id);
 
