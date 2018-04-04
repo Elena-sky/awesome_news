53a9973e8a14a479dcf13c724e123a7fc9b2c1df
MacBook <alonasoroka1993@gmail.com>
2018-04-04 21:15:19 +0300

    comments for news

--- app/Contracts/NewsInterface.php
+++ app/Contracts/NewsInterface.php
@@ -7,6 +7,12 @@ use Illuminate\Http\Request;
 interface NewsInterface
 {
 
+    /**
+     * Get data of the specified news.
+     *
+     * @param $news_id
+     * @return mixed
+     */
     public static function show($news_id);
 
 
--- app/Providers/NewsServiceProvider.php
+++ app/Providers/NewsServiceProvider.php
@@ -9,6 +9,12 @@ use Illuminate\Http\Request;
 
 class NewsServiceProvider implements NewsInterface
 {
+    /**
+     * Get data of the specified news.
+     *
+     * @param $news_id
+     * @return mixed
+     */
     public static function show($news_id)
     {
         return News::find($news_id);
